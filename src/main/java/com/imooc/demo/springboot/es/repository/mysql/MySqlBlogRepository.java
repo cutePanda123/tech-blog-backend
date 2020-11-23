10a4da3498ddfbf1e27ca58c8906be98da675989
Peter Li <peterli1234@126.com>
2020-11-23 14:30:03 -0800

    added cusomized methods to mysql repository

--- src/main/java/com/imooc/demo/springboot/es/repository/mysql/MySqlBlogRepository.java
+++ src/main/java/com/imooc/demo/springboot/es/repository/mysql/MySqlBlogRepository.java
@@ -2,6 +2,16 @@ package com.imooc.demo.springboot.es.repository.mysql;
 
 import com.imooc.demo.springboot.es.entity.mysql.BlogDbEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MySqlBlogRepository extends JpaRepository<BlogDbEntity, Integer> {
+    @Query("select e from BlogDbEntity e order by e.createTime desc")
+    public List<BlogDbEntity> queryAll();
+
+    @Query("select e from BlogDbEntity e where e.title " +
+            "like concat('%',:keyword,'%') or e.content like concat('%',:keyword,'%')")
+    public List<BlogDbEntity> queryWithKeyword(@Param("keyword") String keyword);
 }
